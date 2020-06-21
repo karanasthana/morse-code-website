731222009445d4ac87190043707c87d963bc6357
karanasthana <karan.asthana1@gmail.com>
2020-06-21 21:12:04 +0530

    Updates

---
+++ precache-manifest.c07f253bff7c70bc2725e1adfd9a2ec7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc66265791a2e2e20c0fa3cf8a8268bf",
+    "url": "/morse-code-website/index.html"
+  },
+  {
+    "revision": "4e5a96948fbfdedd9503",
+    "url": "/morse-code-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ccb1e690269beb6f4f93",
+    "url": "/morse-code-website/static/js/2.b5eb8c92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/morse-code-website/static/js/2.b5eb8c92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e5a96948fbfdedd9503",
+    "url": "/morse-code-website/static/js/main.1184655c.chunk.js"
+  },
+  {
+    "revision": "47f087532c194150c5f9",
+    "url": "/morse-code-website/static/js/runtime-main.4c89d8c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/morse-code-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
